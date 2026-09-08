265244f5634fa9158e9f7d8ba820732b0073be19
thabisokhutlane26-oss <thabisokhutlane26@gmail.com>
2026-09-08 17:24:14 +0200

    Update build.gradle.kts with application details

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,31 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.khotla.wallet"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.khotla.wallet"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
 }
